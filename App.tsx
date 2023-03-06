1644906e2cb8b18fca0330f170c2015fe6e13a5d
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-06 20:15:26 -0300

    :lipstick: feat: Style StatusBar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { ThemeProvider } from 'styled-components'
-import theme from './src/theme'
+import { StatusBar } from 'react-native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
-import { Groups } from '@screens/Groups'
+import { ThemeProvider } from 'styled-components'
+import theme from './src/theme'
+
 import { Loading } from '@components/Loading'
+import { Groups } from '@screens/Groups'
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -13,6 +15,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   )
