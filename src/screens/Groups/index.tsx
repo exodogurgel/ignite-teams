29999ceaeaae3a7d151045dc2b0583ae3973b8a2
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-06 21:52:40 -0300

    :sparkles: feat: List groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,13 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos', 'Familia'])
+
   return (
     <Container>
       <Header />
@@ -12,7 +16,15 @@ export function Groups() {
         subtitle='jogue com a sua turma'
       />
 
-      <GroupCard title='Galera do Ignite'/>
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item}
+          />
+        )}
+      />
     </Container>
   );
 }
