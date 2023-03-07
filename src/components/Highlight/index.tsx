b88cfa9cd81264aa1f5395e515ea43ed912e680c
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-06 21:07:47 -0300

    :sparkles: feat: create Highlight component

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,20 @@
+import { Container, SubTitle, Title } from "./styles";
+
+type HighlightProps = {
+  title: string
+  subtitle: string
+}
+
+export function Highlight({ title, subtitle }: HighlightProps) {
+  return (
+    <Container>
+      <Title>
+        {title}
+      </Title>
+
+      <SubTitle>
+        {subtitle}
+      </SubTitle>
+    </Container>
+  )
+}
