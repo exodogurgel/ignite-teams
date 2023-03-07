f7d65619a5f00dd70829a7e2459b8d42dbb031b5
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-07 07:46:35 -0300

    :sparkles: feat: Create NewGroup page

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'styled-components'
 import theme from './src/theme'
 
 import { Loading } from '@components/Loading'
-import { Groups } from '@screens/Groups'
+import { NewGroup } from '@screens/NewGroup'
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -20,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <NewGroup /> : <Loading /> }
     </ThemeProvider>
   )
 }
