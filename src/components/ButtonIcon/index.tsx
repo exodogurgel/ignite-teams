f4246241143897ce54a831ed3bc8ebd842692e44
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-07 20:59:27 -0300

    :sparkles: feat: Create ButtonIcon component

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,19 @@
+import { TouchableOpacityProps } from "react-native";
+import { ButtonIconTypeStyleProps, Container, Icon } from "./styles";
+import { MaterialIcons } from '@expo/vector-icons'
+
+type ButtonIconProps = TouchableOpacityProps & {
+  icon: keyof typeof MaterialIcons.glyphMap 
+  type?: ButtonIconTypeStyleProps
+}
+
+export function ButtonIcon({icon, type = 'PRIMARY', ...props}: ButtonIconProps){
+  return (
+    <Container {...props}>
+      <Icon 
+        name={icon}
+        type={type}  
+      />
+    </Container>
+  )
+}
