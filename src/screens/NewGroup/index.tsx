7e1f9635c67b5e8f17091a671bee6d36ecba74bf
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-08 16:11:49 -0300

    :sparkles: feat: Finishing navigation

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -5,12 +5,15 @@ import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 export function NewGroup() {
+  const [group, setGroup] = useState('')
+
   const navigation = useNavigation()
 
   function handleNew() {
-    navigation.navigate('players', { group: 'Rocket'})
+    navigation.navigate('players', { group })
   }
 
   return (
@@ -27,6 +30,7 @@ export function NewGroup() {
 
         <Input 
           placeholder="Nome da turma"
+          onChangeText={setGroup}
         />
 
         <Button 
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -9,17 +9,25 @@ import { PlayerCard } from "@components/PlayerCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { useRoute } from "@react-navigation/native";
+
+type RouteParams = {
+  group: string
+}
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Êxodo']);
+  const [players, setPlayers] = useState([]);
+
+  const route = useRoute()
+  const { group } = route.params as RouteParams
 
   return (
     <Container>
       <Header showBackButton />
 
       <Highlight
-        title="Nome da turma"
+        title={group}
         subtitle="adicione a galera e separe os times"
       />
 
