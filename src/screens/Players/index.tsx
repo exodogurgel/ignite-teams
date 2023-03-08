d020ab356eec2edbe8a61e6a0f7f84e17ec78ec4
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-08 07:49:13 -0300

    :sparkles: feat: Create PlayerCard component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,15 +1,18 @@
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Button } from "@components/Button";
 import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { ListEmpty } from "@components/ListEmpty";
+import { PlayerCard } from "@components/PlayerCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(['Êxodo']);
 
   return (
     <Container>
@@ -49,6 +52,29 @@ export function Players() {
             {players.length}
         </NumberOfPlayers>
       </HeaderList>
+
+      <FlatList
+          data={players}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <PlayerCard name={item} onRemove={() => {}}/>
+          )}
+          ListEmptyComponent={() => (
+            <ListEmpty 
+              message="Não há pessoas nesse time."
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={[
+            {paddingBottom: 100 },
+            players.length === 0 && { flex: 1 }
+          ]}
+        />
+
+        <Button 
+          title="Remover Turma"
+          type="SECONDARY"
+        />
     </Container>
   )
 }
