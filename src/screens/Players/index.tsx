890199c15dc937254b9e317165dfeffbcfe657bf
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-09 07:46:49 -0300

    :sparkles: feat: Remove group

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,20 +1,25 @@
-import { ButtonIcon } from "@components/ButtonIcon";
-import { Button } from "@components/Button";
-import { Filter } from "@components/Filter";
-import { Header } from "@components/Header";
-import { Highlight } from "@components/Highlight";
-import { Input } from "@components/Input";
-import { ListEmpty } from "@components/ListEmpty";
-import { PlayerCard } from "@components/PlayerCard";
 import { useEffect, useRef, useState } from "react";
 import { Alert, FlatList, TextInput } from "react-native";
-import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
 import { AppError } from "@utils/AppError";
+
 import { playerAddByGroup } from "@storage/player/playerAddByGroup";
-import { playersGetByGroupAndTeam } from "@storage/player/playersGetByGroupAndTeam";
 import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
+import { groupRemoveByName } from "@storage/group/groupRemoveByName";
 import { playerRemoveByGroup } from "@storage/player/playerRemoveByGroup";
+import { playersGetByGroupAndTeam } from "@storage/player/playersGetByGroupAndTeam";
+
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+import { Header } from "@components/Header";
+import { Filter } from "@components/Filter";
+import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
+import { PlayerCard } from "@components/PlayerCard";
+import { ButtonIcon } from "@components/ButtonIcon";
+
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 type RouteParams = {
   group: string
@@ -25,6 +30,7 @@ export function Players() {
   const [team, setTeam] = useState('Time A');
   const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
 
+  const navigation = useNavigation()
   const route = useRoute()
   const { group } = route.params as RouteParams
 
@@ -77,6 +83,29 @@ export function Players() {
       console.log(error)
       Alert.alert('Remover pessoa', 'Não foi possível remover essa pessoa.')
     }
+  }
+
+  async function groupRemove() {
+    try {
+      await groupRemoveByName(group)
+
+      navigation.navigate('groups')
+
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Remover grupo', 'Não foi possível remover o grupo.')
+    }
+  }
+
+  async function handleGroupRemove() {
+    Alert.alert(
+      'Remover',
+      'Deseja remover o grupo?',
+      [
+        { text: 'Não', style: 'cancel'},
+        { text: 'Sim', onPress: () => groupRemove() },
+      ]
+    )
   }
 
   useEffect(() => {
@@ -151,6 +180,7 @@ export function Players() {
         <Button 
           title="Remover Turma"
           type="SECONDARY"
+          onPress={handleGroupRemove}
         />
     </Container>
   )
